57a9d6fc87e6aa3fc12d41c47eeb32720587f109
Kris Nova <kris@nivenly.com>
2017-08-28 08:18:09 -0600

    Update freebsd-create-vm.sh

--- 2-as-script/freebsd-create-vm.sh
+++ 2-as-script/freebsd-create-vm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-pkg install bash -y
+pkg install -y bash nano
 curl -L https://aka.ms/InstallAzureCli | bash
 az login
 az group delete -n infra-2 --yes
 az group create --name infra-2 --location eastus
 az vm create --resource-group infra-2 --name infra-2 --image UbuntuLTS --ssh-key-value "ssh-rsa 
 AAAAB3NzaC1yc2EAAAADAQABAAABAQC34QGJe1+MkcimmQmiRgZiZX5y7k/z7Y4W2H0WTnh/WhKkUvjRgWcd3A0kUb5MIW9zbRzBFIJBEBrm8HRlF4enXRACr+tVbHUqI5JO3xIEHKWkJHGf0Bg43+VW8X/7ftP7QPe5imet5TL+AqsC62Wq2aUTZMHag1R6xv4AGHHPNx/dbbRn26Mtrc8Jh4WJXtYfk3CDU46zHHUW+VInuDMFmtW4ykeyPqBaKQvrjr+XQm15vfsrtoaBG4JweKOD/LJLs+tiKtsVMf55sWZxdPdHLtO1/Rsb+Z+1wSeT8mM1tBanigShxmJeC0gZ8HQOO2OveF0jYUjMK4wPwGryPZkf" --admin-username kris
-
